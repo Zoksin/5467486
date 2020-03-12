b78177bf86baf17c0c798c240f7fc2580ee871d8
Zoksin <56645445+Zoksin@users.noreply.github.com>
2020-03-12 19:17:38 +0300

    Add files via upload

---
+++ MyCalc.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using MyCalcLib;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace MyCalcLib
+{
+    public class MyCalc
+    {
+        public bool Sum(string x, char[] w, string q7)
+        {
+            string letter = "ABEKMHOPCTYXАВЕКМНОРСТУХ";
+
+            if (letter.Contains(w[0]) && Char.IsDigit(w[1]) && Char.IsDigit(w[2]) && Char.IsDigit(w[3]) && letter.Contains(w[4]) && letter.Contains(w[5]) && (q7 == "11" || q7 == "22" || q7 == "33" || q7 == "44" || q7 == "55" || q7 == "111"))
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
---
+++ MyCalcTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyCalcLib.Tests
+{
+    [TestClass]
+    public class MyCalcTests
+    {
+        [TestMethod]
+        public void Sum_10and20_30return()
+        {
+            string x = "A111AA11";
+
+            char[] w = x.ToCharArray();
+
+            string q7 = x.Substring(6, x.Length - 6);
+
+            MyCalc c = new MyCalc();
+            bool ac = c.Sum(x, w, q7);
+
+            Assert.AreEqual(true, ac);
+        }
+    }
+}
